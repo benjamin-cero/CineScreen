e6f408b5089b13ae6b71fcde2aae44228ef0dbb6
benjamin-cero <benjamin.cero25@gmail.com>
2024-11-01 03:11:43 +0100

    Moduli
    Dodavanje i miksanje vise vrsta modula

--- CineScreen/src/app/app.component.ts
+++ CineScreen/src/app/app.component.ts
@@ -1,26 +1,26 @@
 import {Component, Input, input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgIf, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name:string = "Benjamin"
   getname():string{
-    let r = Math.random();
-    return "HELLO " + this.name + " " + r;
+    return "HELLO " + " " + this.name;
   }
   brojac:number=0;
   button1click():void{
     this.name += ("." + this.brojac++);
   }
-  nameChangeEvent($event: Event) {
-    // @ts-ignore
-    let v= $event.target.value;
-
+niz:string[] = ["jedan", "dva", "tri", "cetri"];
+  showHeader() {
+    return this.name.length>2;
   }
 }
