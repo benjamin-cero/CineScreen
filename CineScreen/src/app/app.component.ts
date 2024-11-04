2a3ccbd65d0dc570542985456616dfea4e8c4bb6
benjamin-cero <benjamin.cero25@gmail.com>
2024-11-04 01:19:17 +0100

    Color Background

--- CineScreen/src/app/app.component.ts
+++ CineScreen/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component, Input, input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import {NgForOf, NgIf} from '@angular/common';
+import {NgForOf, NgIf, NgStyle} from '@angular/common';
+import {argsArgArrayOrObject} from 'rxjs/internal/util/argsArgArrayOrObject';
+import {colors} from '@angular/cli/src/utilities/color';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgIf, NgForOf],
+  imports: [RouterOutlet, FormsModule, NgIf, NgForOf, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -23,4 +25,7 @@ niz:string[] = ["jedan", "dva", "tri", "cetri"];
   showHeader() {
     return this.name.length>2;
   }
+
+  protected readonly argsArgArrayOrObject = argsArgArrayOrObject;
+  protected readonly colors = colors;
 }
