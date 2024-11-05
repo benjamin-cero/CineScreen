5d3f4903bed34758ae670ed815ca33c3069eb08a
benjamin-cero <benjamin.cero25@gmail.com>
2024-11-05 01:31:42 +0100

    HttpClient

--- CineScreen/src/app/app.component.ts
+++ CineScreen/src/app/app.component.ts
@@ -2,14 +2,19 @@ import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule} from '@angular/forms';
 import {NgForOf, NgIf, NgStyle} from '@angular/common';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgIf, NgForOf, NgStyle],
+  imports: [RouterOutlet, FormsModule, NgIf, NgForOf, NgStyle,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+ constructor(private HttpClient: HttpClient) {
+
+ }
+
   name:string = "Benjamin"
    temp: string = "";
   getname():string{
@@ -24,19 +29,20 @@ niz:string[] = ["jedan", "dva", "tri", "cetri"];
     return this.name.length>2;
   }
 
-
   ChangeColors() {
     return this.name.startsWith('A')?{backgroundColor:'Red'}:{backgroundColor:'Blue'};
   }
 
   fetchData() {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=Mostar&appid=917b026a997320574cd4315b9bf4c73a   `)
+    const url = 'http://api.openweathermap.org/data/2.5/weather?q=Mostar&appid=917b026a997320574cd4315b9bf4c73a'
+    /*fetch(`http://api.openweathermap.org/data/2.5/weather?q=Mostar&appid=917b026a997320574cd4315b9bf4c73a   `)
       .then(response => {
         response.json().then(pr => {
-          this.temp = (pr.main.temp - 273).toFixed(1);
-
-
+          ;
         })
-      })
+      })*/
+    this.HttpClient.get(url).subscribe((x:any) => {
+      this.temp = (x.main.temp - 273).toFixed(1)
+    });
   }
 }
