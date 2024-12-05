58d19d8d5a1abd831cfb43c0f60320c5e0b12a13
benjamin-cero <benjamin.cero25@gmail.com>
2024-12-05 02:36:51 +0100

    CRUD for Actor Controller

---
+++ backend/FIT_Api_Example/ViewModel/ActorUpsertVM.cs
@@ -0,0 +1,8 @@
+﻿namespace FIT_Api_Example.ViewModel
+{
+    public class ActorUpsertVM
+    {
+        public string FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
