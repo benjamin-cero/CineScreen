56d997f865fee965adddb9e034d126d966f5cb40
benjamin-cero <benjamin.cero25@gmail.com>
2025-11-25 04:00:49 +0100

    Actor get by id endpoint

---
+++ backend/CineScreen/Endpoints/ActorEndpoints/ActorGetByIdEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using CineScreen.Data;
+using CineScreen.Helper.Api;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static CineScreen.Endpoints.ActorEndpoints.ActorGetByIdEndpoint;
+
+namespace CineScreen.Endpoints.ActorEndpoints;
+
+[Route("actors")]
+public class ActorGetByIdEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithActionResult<ActorGetByIdResponse>
+{
+    [HttpGet("{id}")]
+    public override async Task<ActionResult<ActorGetByIdResponse>> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var actor = await db.Actors
+                            .Where(a => a.ID == id)
+                            .Select(a => new ActorGetByIdResponse
+                            {
+                                ID = a.ID,
+                                FirstName = a.FirstName,
+                                LastName = a.LastName
+                            })
+                            .FirstOrDefaultAsync(x => x.ID == id, cancellationToken);
+
+        if (actor == null)
+        {
+            return NotFound("Actor not found");
+        }
+
+
+        return Ok(actor);
+    }
+
+    public class ActorGetByIdResponse
+    {
+        public required int ID { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
+
+    }
+}
