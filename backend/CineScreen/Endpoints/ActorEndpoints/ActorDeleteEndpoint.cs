64ba7944b8cae691e421c77c09b7080d54df73f3
benjamin-cero <benjamin.cero25@gmail.com>
2025-11-25 04:00:03 +0100

    Actor Delete endpoint

---
+++ backend/CineScreen/Endpoints/ActorEndpoints/ActorDeleteEndpoint.cs
@@ -0,0 +1,31 @@
+﻿namespace RS1_2024_25.API.Endpoints.ActorEndpoints;
+
+using CineScreen.Data;
+using CineScreen.Helper.Api;
+using CineScreen.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+//[MyAuthorization(isAdmin: true, isUser: false)]
+[Route("actors")]
+public class ActorDeleteEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
+    .WithRequest<int>
+    .WithoutResult
+{
+
+    [HttpDelete("{id}")]
+    public override async Task HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var actor = await db.Actors.SingleOrDefaultAsync(x => x.ID == id, cancellationToken);
+
+        if (actor == null)
+            throw new KeyNotFoundException("Actor not found");
+
+
+        db.Actors.Remove(actor);
+        await db.SaveChangesAsync(cancellationToken);
+    }
+}
+
