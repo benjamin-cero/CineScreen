9d3017c1d199a8f580ba1788d4b7b23303b8a04a
benjamin-cero <benjamin.cero25@gmail.com>
2025-11-26 16:52:14 +0100

    Actor update velidator

---
+++ backend/CineScreen/Endpoints/ActorEndpoints/ActorUpdateOrInsertValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace CineScreen.Endpoints.ActorEndpoints;
+
+using FluentValidation;
+using static CineScreen.Endpoints.ActorEndpoints.ActorUpdateOrInsertEndpoint;
+
+public class ActorUpdateOrInsertValidator : AbstractValidator<ActorUpdateOrInsertEndpoint.ActorUpdateOrInsertRequest>
+{
+    public ActorUpdateOrInsertValidator()
+    {
+
+        RuleFor(x => x.FirstName)
+            .NotEmpty().WithMessage("First name is required")
+            .Matches(@"^[a-zA-ZÀ-ÿ\s]+$").WithMessage("First name can only contain letters and spaces")
+            .MaximumLength(50).WithMessage("First name cannot exceed 50 characters");
+    
+    }
+}
