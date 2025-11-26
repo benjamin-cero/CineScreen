e505bdb3c9ef90a9e579bbd70b196ef7b3b17b8f
benjamin-cero <benjamin.cero25@gmail.com>
2025-11-26 16:54:30 +0100

    City delete endpoint

--- backend/CineScreen/Endpoints/CityEndpoints/CityDeleteEndpoint.cs
+++ backend/CineScreen/Endpoints/CityEndpoints/CityDeleteEndpoint.cs
@@ -1,31 +1,32 @@
-﻿namespace RS1_2024_25.API.Endpoints.CityEndpoints;
-
 using CineScreen.Data;
 using CineScreen.Helper.Api;
-using CineScreen.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Threading;
-using System.Threading.Tasks;
+using static CineScreen.Endpoints.CityEndpoints.CityDeleteEndpoint;
 
-[MyAuthorization(isAdmin: true, isUser : false)]
+namespace CineScreen.Endpoints.CityEndpoints;
+
 [Route("cities")]
 public class CityDeleteEndpoint(ApplicationDbContext db) : MyEndpointBaseAsync
-    .WithRequest<int>
+    .WithRequest<CityDeleteRequest>
     .WithoutResult
 {
-
     [HttpDelete("{id}")]
-    public override async Task HandleAsync(int id, CancellationToken cancellationToken = default)
+    public override async Task HandleAsync([FromRoute] CityDeleteRequest request, CancellationToken cancellationToken = default)
     {
-        var city = await db.Cities.SingleOrDefaultAsync(x => x.ID == id, cancellationToken);
+        var city = await db.Cities.SingleOrDefaultAsync(x => x.ID == request.ID, cancellationToken);
 
         if (city == null)
+        {
             throw new KeyNotFoundException("City not found");
-
+        }
 
         db.Cities.Remove(city);
         await db.SaveChangesAsync(cancellationToken);
     }
-}
 
+    public class CityDeleteRequest
+    {
+        public int ID { get; set; }
+    }
+} 
