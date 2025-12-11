7f744d675fb8e5b892869a53abed4637d20767bc
benjamin-cero <benjamin.cero25@gmail.com>
2025-12-11 04:04:15 +0100

    Movie director get all endpoint

---
+++ frontend/src/app/endpoints/movie-director-endpoints/movie-director-get-all-endpoint.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {MyPagedRequest} from '../../helper/my-paged-request';
+import {MyConfig} from '../../my-config';
+import {buildHttpParams} from '../../helper/http-params.helper';
+import {MyBaseEndpointAsync} from '../../helper/my-base-endpoint-async.interface';
+import {MyPagedList} from '../../helper/my-paged-list';
+import {MyCacheService} from '../../services/cache-service/my-cache.service';
+import {of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+export interface MovieDirectorGetAllRequest extends MyPagedRequest {
+  q?: string;
+}
+
+export interface MovieDirectorGetAllResponse {
+  id: number;
+  movieId: number;
+  directorId: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieDirectorGetAllEndpointService implements MyBaseEndpointAsync<MovieDirectorGetAllRequest, MyPagedList<MovieDirectorGetAllResponse>> {
+  private apiUrl = `${MyConfig.api_address}/movie-directors/filter`;
+
+  constructor(private httpClient: HttpClient, private cacheService: MyCacheService) {}
+
+  handleAsync(request: MovieDirectorGetAllRequest, useCache: boolean = false, cacheTTL: number = 300000) {
+    const cacheKey = `${request.q || ''}-${request.pageNumber || 1}-${request.pageSize || 10}`;
+    if (useCache && this.cacheService.has(cacheKey)) {
+      let data = this.cacheService.get<MyPagedList<MovieDirectorGetAllResponse>>(cacheKey)!;
+      return of(data);
+    }
+    const params = buildHttpParams(request);
+    return this.httpClient.get<MyPagedList<MovieDirectorGetAllResponse>>(`${this.apiUrl}`, {params}).pipe(
+      tap((data) => {
+        if (useCache) {
+          this.cacheService.set(cacheKey, data, cacheTTL);
+        }
+      })
+    );
+  }
+} 
