91a074b5df2713298458dd9c7db4a99e786a0f3d
benjamin-cero <benjamin.cero25@gmail.com>
2025-12-17 01:10:13 +0100

    client routing

--- frontend/src/app/modules/client/client-routing.module.ts
+++ frontend/src/app/modules/client/client-routing.module.ts
@@ -1,9 +1,18 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ReservationComponent} from './reservation/reservation.component';
+import {ProfilePageComponent} from './profile-page/profile-page.component';
+import {SettingsComponent} from './settings/settings.component';
+import {ReservationsComponent} from './reservations/reservations.component';
+import { ClientMenusComponent } from './menus/menus.component';
 
 const routes: Routes = [
   {path: 'reservation', component: ReservationComponent},
+  {path: 'profile', component: ProfilePageComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'reservations', component: ReservationsComponent},
+  {path: 'menus', component: ClientMenusComponent},
+  {path: '', redirectTo: 'profile', pathMatch: 'full'}, // Default route
 ];
 
 @NgModule({
