fc5030b150a372f0a9fcbdda26df4681679be174
benjamin-cero <benjamin.cero25@gmail.com>
2025-12-17 01:10:30 +0100

    Client module ts

--- frontend/src/app/modules/client/client.module.ts
+++ frontend/src/app/modules/client/client.module.ts
@@ -3,15 +3,49 @@ import { CommonModule } from '@angular/common';
 
 import { ClientRoutingModule } from './client-routing.module';
 import { ReservationComponent } from './reservation/reservation.component';
-
+import { ProfilePageComponent } from './profile-page/profile-page.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ReservationsComponent } from './reservations/reservations.component';
+import { TranslateModule } from '@ngx-translate/core';
+import { ClientMenusComponent } from './menus/menus.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
-    ReservationComponent
+    ReservationComponent,
+    ProfilePageComponent,
+    SettingsComponent,
+    ReservationsComponent,
+    ClientMenusComponent
   ],
   imports: [
     CommonModule,
-    ClientRoutingModule
+    ClientRoutingModule,
+    TranslateModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ]
 })
 export class ClientModule { }
